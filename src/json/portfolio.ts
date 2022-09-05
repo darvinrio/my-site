e337bc69b73bf031a3c9be49b0429d1833553845
Darvin Rio <todarvinrio@gmail.com>
2022-09-06 00:27:00 +0530

    added tool icons and cleaned portfolio page

--- src/json/portfolio.ts
+++ src/json/portfolio.ts
@@ -1,6 +1,7 @@
-import ghostpng from "../imgs/ghost.png";
-import arblunapng from "../imgs/arbluna.png";
-import hyphenminepng from "../imgs/hyphenmine.png";
+import ghostpng from "../imgs/portfolio/ghost.png";
+import arblunapng from "../imgs/portfolio/arbluna.png";
+import hyphenminepng from "../imgs/portfolio/hyphenmine.png";
+import hyarbpng from "../imgs/portfolio/hyarb.png";
 
 export const portfolio = [
   {
@@ -8,17 +9,27 @@ export const portfolio = [
     img: ghostpng,
     desc: "A etherscan like block explorer for AAVE",
     href: "https://ghost-hunter-aave.herokuapp.com/",
+    icons: ['react','js','gql']
   },
   {
     header: "Hyphen Bridge",
     img: hyphenminepng,
-    desc: "Tracking hyphen bridge activity on 6 different chains",
+    desc: "Dashboard tracking Hyphen bridge activity on 6 different chains",
     href: "https://bico-hyphen.vercel.app/",
+    icons: ['next','js','sql']
   },
   {
     header: "Luna Arbitrage Aggregator",
     img: arblunapng,
     desc: "[now defunct] Aggregates LUNA derivatives arbitrage opportunities",
     href: "https://luna-arb.herokuapp.com/",
+    icons: ['react','js','jsonrpc']
+  },
+  {
+    header: "Hyphen on Arbitrum",
+    img: hyarbpng,
+    desc: "Flipside Dashboard tracking Hyphen bridge activity on Arbitrum",
+    href: "https://app.flipsidecrypto.com/dashboard/hyphen-bridge-on-arbitrum-oSUXhe",
+    icons: ['sql','fs']
   },
 ];
